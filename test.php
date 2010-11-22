c2d3d01d8a2fc7d2ba259b648a5651762ae2f761
lch <vnux@nuxos.local>
2010-11-22 22:46:15 +0100

    Add Dailymotion provider. Test first git commit. Please ignore

---
+++ test.php
@@ -0,0 +1,36 @@
+<?php
+
+
+// Include the autoloader.
+require_once( dirname( __FILE__ ) . '/Classes/Autoloader.class.php' );
+
+// Register the autoloader.
+RRoEmbed_Autoloader::getInstance()->register();
+
+// Create a new consumer.
+$c = new RRoEmbed_Consumer();
+
+// Call the "consume" method and supply it the URL of the resource you'd like
+// to get the oEmbed representation of and the Provider instance.
+//$o = $c->consume(
+//	'http://www.flickr.com/photos/romac17/4101838222/',
+//	new RRoEmbed_Provider_Flickr()
+//);
+
+//$o = $c->consume(
+//	'http://vimeo.com/15952335',
+//	new RRoEmbed_Provider_Vimeo()
+//);
+
+$o = $c->consume(
+	'http://www.dailymotion.com/video/xf02xp_uffie-difficult_music',
+	new RRoEmbed_Provider_Dailymotion()
+);
+
+
+// Notice you can also omit to specify the provider, if so the Consumer will try to
+// automatically find a valid provider endpoint.
+// $o = $c->consume( 'http://www.flickr.com/photos/romac17/4101838222/' );
+
+print $o;
+ 
